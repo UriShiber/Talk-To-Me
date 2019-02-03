4e40945462f86e47667a9ce1d0476b040c822c84
UriShiber <35705607+UriShiber@users.noreply.github.com>
2019-02-03 23:28:51 +0200

    Update try1.py

--- final_proj/try1.py
+++ final_proj/try1.py
@@ -1,31 +1,124 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import hilbert, chirp
 
-duration = 1.0
-fs = 400.0
-samples = int(fs*duration)
-t = np.arange(samples) / fs
-signal = chirp(t, 20.0, t[-1], 100.0)
-signal *= (1.0 + 0.5 * np.sin(2.0*np.pi*3.0*t))
-analytic_signal = hilbert(signal)
-# envelope --------------------------------------------------------------------
-amplitude_envelope = np.abs(analytic_signal)
 
-fig = plt.figure()
-ax0 = fig.add_subplot(211)
-ax0.plot(t, analytic_signal, label='analytic signal')
-# ax0.plot(t, signal, label='signal')
-ax0.plot(t, amplitude_envelope, label='envelope')
-ax0.set_xlabel("time in seconds")
-"""
-instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-instantaneous_frequency = np.diff(instantaneous_phase) / (2.0*np.pi) * fs
-ax1 = fig.add_subplot(212)
-ax1.plot(t[1:], instantaneous_frequency)
-ax1.set_xlabel("time in seconds")
-ax1.set_ylim(0.0, 120.0)
-"""
+# imports
+import socket
+import select
 
-plt.show()
+
+class Server:
+    """
+        this class has 2 parameters - ip and port
+        which define the socket. in this class we have a constructor,
+        we have functions on the socket and handling the client's request
+    """
+    def __init__(self, ip, port):
+        """
+        constructor
+        :param ip:
+        :param port:
+        """
+        self.ip = ip
+        self.port = port
+
+    def bind(self, a_server_socket):
+        """
+        building the connection with the socket
+        :param a_server_socket:
+        """
+        a_server_socket.bind((self.ip, self.port))
+
+    @staticmethod
+    def listen(a_server_socket, num):
+        """
+        listens to the client's request
+        :param a_server_socket:
+        :param num:
+        """
+        a_server_socket.listen(num)
+
+    @staticmethod
+    def accept(a_server_socket):
+        """
+        accept the request of the client
+        :param a_server_socket:
+        """
+        return a_server_socket.accept()
+
+    @staticmethod
+    def send(client1_socket, message):
+        """
+        send a message to the client
+        :param client1_socket:
+        :param message:
+        """
+        client1_socket.send(message)
+
+    @staticmethod
+    def receive(client_socket, data):
+        """
+        receive data from the client
+        :param client_socket:
+        :param data:
+        :return:
+        """
+        return client_socket.recv(data)
+
+
+def send_waiting_messages(w_list, messages_to_send):
+    """
+    sends numbers start option and end option to each thread
+    :param w_list:
+    :param messages_to_send:
+    :return:
+    """
+    for msg in messages_to_send:
+        client_socket, num = msg
+        if client_socket in w_list:
+            client_socket.send(str(num).encode())
+            messages_to_send.remove(msg)
+
+
+def main():
+    """
+    the main, here we can see the object of Server
+    and the calls to the functions
+    """
+    # open socket with client
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # create an object
+    my_server = Server('192.168.1.20', 1212)
+    my_server.bind(server_socket)
+    my_server.listen(server_socket, 0)
+    open_client_sockets = []
+    messages_to_send = []
+    byte_arr = []
+    flag = True
+    i = 0
+    while flag:
+        r_list, w_list, x_list = select.select([server_socket] +
+                                               open_client_sockets,
+                                               open_client_sockets, [])
+        for current_socket in r_list:
+            if current_socket is server_socket:
+                new_socket, address = server_socket.accept()
+                open_client_sockets.append(new_socket)
+            else:
+                data = my_server.receive(current_socket, 4096)
+                byte_arr.append(data)
+                messages_to_send.append((current_socket, 1))
+                if str(data) == "b''":
+                    f = open('my_record3', 'wb')
+                    while i < byte_arr.__len__():
+                        f.write(byte_arr[i])
+                        print(byte_arr[i], i)
+                        i += 1
+                    f.close()
+                    open_client_sockets.remove(current_socket)
+                    print("Connection with client closed")
+         # send_waiting_messages(w_list, messages_to_send)
+    server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
 
