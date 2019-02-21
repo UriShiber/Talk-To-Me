8a467bbf4bc3970ae5effca3064c1c67882536fc
UriShiber <35705607+UriShiber@users.noreply.github.com>
2019-02-21 20:14:26 +0200

    Update convert audio file to wave.py

--- final_proj/work/convert audio file to wave.py
+++ final_proj/work/convert audio file to wave.py
@@ -1,4 +1,6 @@
-
+"""
+needs to move the ffmpeg to the same dir as this file
+"""
 from pydub import AudioSegment
 import os
 import wave
@@ -7,26 +9,29 @@ RECORDING_DIR = r"C:\final_proj\Recordings"
 FILES_TO_CONVERT_DIR = r"C:\final_proj\Files_To_Convert"
 
 
-def convert_m4a_to_wav(file_name):
+def convert_file_to_wav(file_name):
     """
     will be attached later after i will see the type of the file i will get from
     the front - end
     :param file_name:
     :return:
     """
-    counter = 17
+    counter = 1
     wave_file = wave.open(r"C:\final_proj\Recordings\record_try_1.wav", 'rb')
-    # for file in os.listdir(RECORDING_DIR):
-        # if os.path.isfile(os.path.join(RECORDING_DIR, file)):
-            # counter += 1
+    for file in os.listdir(RECORDING_DIR):
+        if os.path.isfile(os.path.join(RECORDING_DIR, file)):
+            counter += 1
 
     audio_signal_segment = AudioSegment.from_file(file_name)
-    audio_file = os.path.join(RECORDING_DIR, "record_try_{0}.wav".format(counter))
-    audio_signal_segment = audio_signal_segment.set_channels(wave_file.getnchannels())
-    audio_signal_segment = audio_signal_segment.set_frame_rate(wave_file.getframerate())
-    audio_signal_segment = audio_signal_segment.set_sample_width(wave_file.getsampwidth())
+    audio_file = os.path.join(RECORDING_DIR, "record_try_{0}.wav".
+                              format(counter))
+    audio_signal_segment = audio_signal_segment.set_channels(wave_file.
+                                                             getnchannels())
+    audio_signal_segment = audio_signal_segment.set_frame_rate(wave_file.
+                                                               getframerate())
+    audio_signal_segment = audio_signal_segment.set_sample_width(wave_file.
+                                                                 getsampwidth())
     audio_signal_segment.export(audio_file, format='wav')
     return audio_file
 
 
-audio_file2 = convert_m4a_to_wav(r"C:\final_proj\my_record1")
